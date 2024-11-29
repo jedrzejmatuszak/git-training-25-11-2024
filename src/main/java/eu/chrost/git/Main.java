e78706450ceaaf28fe710a88af0286ebba75aae5
Jędrzej Matuszak <jedrzej.matuszak@gmail.com>
2024-11-29 09:37:26 +0100

    added scanner

--- src/main/java/eu/chrost/git/Main.java
+++ src/main/java/eu/chrost/git/Main.java
@@ -1,8 +1,14 @@
 package eu.chrost.git;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("text");
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        scanner.close();
     }
 }
