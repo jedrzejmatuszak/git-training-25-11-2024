b4bbcfeac33d999fa864078d838afe1a6140552a
Jędrzej Matuszak <jedrzej.matuszak@gmail.com>
2024-11-29 10:17:23 +0100

    feature 2

--- src/main/java/eu/chrost/git/Feature.java
+++ src/main/java/eu/chrost/git/Feature.java
@@ -3,5 +3,6 @@ package eu.chrost.git;
 public class Feature {
     public static void main(String[] args) {
         System.out.println("new feature");
+        System.out.println("new feature 2");
     }
 }
